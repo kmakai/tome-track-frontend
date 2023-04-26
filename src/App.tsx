fea8924cf93a9a944a156bd04a205f568c776c53
kmakai <makaikwag@gmail.com>
2023-04-26 19:39:21 -0400

    intial setup for frontend

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import "./index.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline text-red-700">
+        Welcome to Tome-Track
+      </h1>
+    </>
+  );
+}
+
+export default App;
